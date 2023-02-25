38a2625b143287b51a58dbf86ca5fad474c93373
Mateus Sousa <mateusdevsousa@gmail.com>
2023-02-25 02:43:53 -0300

    Fix indentation

--- Package.swift
+++ Package.swift
@@ -3,16 +3,16 @@
 import PackageDescription
 
 let package = Package(
-    name: "SyntaxAnalyzer",
+	name: "SyntaxAnalyzer",
 	platforms: [.macOS(.v10_15)],
-    products: [
+	products: [
 		.executable(name: "SyntaxAnalyzer", targets: ["SyntaxAnalyzer"]),
-    ],
+	],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-syntax.git", branch: "main"),
 		.package(url: "https://github.com/apple/swift-argument-parser", from: "1.0.0")
 	],
-    targets: [
+	targets: [
 		.executableTarget(
 			name: "SyntaxAnalyzer",
 			dependencies: [
@@ -20,9 +20,9 @@ let package = Package(
 				.product(name: "ArgumentParser", package: "swift-argument-parser"),
 			],
 			path: "Sources"),
-        .testTarget(
-            name: "SyntaxAnalyzerTests",
+		.testTarget(
+			name: "SyntaxAnalyzerTests",
 			dependencies: ["SyntaxAnalyzer"],
 			path: "Tests"),
-    ]
+	]
 )
